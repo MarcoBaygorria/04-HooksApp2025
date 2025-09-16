19eaaff9ef22ae05eaf6cb57501a41a03ed094b0
MarcoBaygorria <baygorria91@gmail.com>
2025-09-15 22:40:40 -1100

    useState

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { HokksApp } from './HokksApp.js'
+// import { HokksApp } from './HokksApp.js'
+import { TrafficLight } from './01-useState/TrafficLght.js'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HokksApp />
+    <TrafficLight />
   </React.StrictMode>,
 )
