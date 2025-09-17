4ca8bb50fad4e0fa13fe0d5a49899b908e3a1120
MarcoBaygorria <baygorria91@gmail.com>
2025-09-16 14:25:12 -1100

    useEffect y customhook

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 // import { HokksApp } from './HokksApp.js'
-import { TrafficLight } from './01-useState/TrafficLght.js'
+// import { TrafficLight } from './01-useState/TrafficLght.js'
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect.js'
+import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook.js'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <TrafficLight />
+    {/* <TrafficLight /> */}
+    {/* <TrafficLightWithEffect /> */}
+    <TrafficLightWithHook />
   </React.StrictMode>,
 )
